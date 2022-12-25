8619b9fbfd4652d7c28f34bffdbc2de0bcd01b9c
vinaykanth97 <vinaykennedy@gmail.com>
2022-12-25 23:13:32 +0530

    updated Registration Page

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,45 +2,125 @@ import app from '../FirebaseConfig';
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth'
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import { Card, Container, Typography } from '@mui/material';
+import { Alert, Card, Container, Typography } from '@mui/material';
 import { CircularProgress, Button } from '@mui/material';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useRef, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useForm, Controller } from "react-hook-form";
 
 export default function Register() {
     const authentication = getAuth(app);
-    const [registerField, setregisterField] = useState([])
-    const HandleRegisterChange = (e) => {
-        setregisterField({
-            ...registerField,
-            [e.target.name]: e.target.value
+    const navigate = useNavigate();
+    const { handleSubmit, control, getValues } = useForm();
+    const [registrationStatus, setRegistrationStatus] = useState({
+        loading: false,
+        submission: null
+    })
+    const registrationRef = useRef(null)
+    const onSubmit = data => {
+        // setRegistrationStatus(true)
+        setRegistrationStatus({
+            loading: true,
+            submission: null
         })
-    }
-    const HandleRegistration = (e) => {
-        e.preventDefault()
-        const { email, password, confirmpassword } = registerField
-        createUserWithEmailAndPassword(authentication, email, password).then(userCredentials => {
-            const user = userCredentials.user
-            console.log(user)
-        }).catch(err => {
-            console.log(err)
+        const { email, password } = data
+        createUserWithEmailAndPassword(authentication, email, password).then(_userCredentials => {
+            setRegistrationStatus({
+                loading: false,
+                submission: true
+            })
+            setTimeout(() => {
+                navigate("/login");
+            }, 3000)
+        }).catch(_err => {
+            setRegistrationStatus({
+                loading: false,
+                submission: false
+            })
         })
-    }
+        registrationRef.current.reset()
+    };
     return (
         <div className="register centered-form">
             <Container>
                 <Card variant='outlined' sx={{ width: 400, mx: "auto", padding: 4 }}>
+
                     <Typography variant="h4" component="h4" align='center' sx={{ mb: 3 }}>
                         Register
                     </Typography>
-                    <form onSubmit={HandleRegistration}>
-                        <TextField name='email' label="Email" variant="outlined" sx={{ width: 1, mb: 2 }}
-                            required onChange={HandleRegisterChange} />
-                        <TextField name='password' type="password" label="Password" variant="outlined" sx={{ width: 1, mb: 2 }} required onChange={HandleRegisterChange} />
-                        <TextField name='confirmpassword' type="password" label="Confirm Password" variant="outlined" sx={{ width: 1, mb: 4 }} required onChange={HandleRegisterChange} />
+                    <form onSubmit={handleSubmit(onSubmit)} ref={registrationRef}>
+                        <Controller
+                            name="email"
+                            control={control}
+                            defaultValue=""
+                            rules={{
+                                required: 'Email required', pattern: {
+                                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                    message: "invalid email address"
+                                }
+                            }}
+                            render={({ field: { onChange, value }, fieldState: { error } }) => (
+                                <TextField
+                                    name='email'
+                                    label="Email"
+                                    variant="outlined"
+                                    value={value}
+                                    sx={{ width: 1, mb: 2 }}
+                                    onChange={onChange}
+                                    error={!!error}
+                                    helperText={error ? error.message : null}
+                                />
+                            )}
+                        />
+                        <Controller
+                            name="password"
+                            control={control}
+                            defaultValue=""
+                            rules={{
+                                required: 'Password must be entered',
+                                minLength: {
+                                    value: 6,
+                                    message: "Password must have at least 6 characters"
+                                }
+                            }}
+                            render={({ field: { onChange, value }, fieldState: { error } }) => (
+                                <TextField
+                                    type='password'
+                                    name='password'
+                                    label="Password"
+                                    variant="outlined"
+                                    value={value}
+                                    sx={{ width: 1, mb: 2 }}
+                                    onChange={onChange}
+                                    error={!!error}
+                                    helperText={error ? error.message : null}
+
+                                />
+                            )}
+                        />
+                        <Controller
+                            name="confirmpassword"
+                            control={control}
+                            defaultValue=""
+                            rules={{ validate: value => (value === getValues().password || 'Password does not match') }}
+                            render={({ field: { onChange, value }, fieldState: { error } }) => (
+                                <TextField
+                                    type='password'
+                                    name='confirmpassword'
+                                    label="Confirm Password"
+                                    variant="outlined"
+                                    value={value}
+                                    sx={{ width: 1, mb: 2 }}
+                                    onChange={onChange}
+                                    error={!!error}
+                                    helperText={error ? error.message : null}
+                                />
+                            )}
+                        />
                         <Button variant="outlined" type='submit'>
+                            {registrationStatus.loading ? <CircularProgress size={14} /> : 'Register'}
                             {/* <CircularProgress size={14} /> */}
-                            Register
+
                         </Button>
                     </form>
                     <Typography variant="p" component="p" sx={{ mt: 3 }}>
@@ -48,6 +128,8 @@ export default function Register() {
                     </Typography>
                 </Card>
             </Container>
-        </div>
+            {registrationStatus.submission ? <Alert severity="success" className='badge-status'>Successfully created your account. We're redirecting you to the login page shortly!</Alert> : registrationStatus.submission === false ? <Alert severity="error" className='badge-status'>This account already exists! Kindly Login</Alert> : ''}
+
+        </div >
     )
 }
