b66ee88820a3c13533c98a7e18e60bd7002c2721
vinaykanth97 <vinaykennedy@gmail.com>
2022-12-25 17:18:46 +0530

    building loginpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
-
-
+import Login from './components/Login';
+import Register from './components/Register';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 function App() {
   return (
-   <></>
+    <>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <BrowserRouter>
+          <Routes>
+            <Route path='/register' element={<Register />}></Route>
+            <Route index path='/login' element={<Login />}></Route>
+            <Route path="/" element={<Navigate to="/login" />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </>
   );
 }
 
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,18 +1,53 @@
 import app from '../FirebaseConfig';
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { Card, Container, Typography } from '@mui/material';
+import { CircularProgress, Button } from '@mui/material';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 export default function Register() {
     const authentication = getAuth(app);
+    const [registerField, setregisterField] = useState([])
+    const HandleRegisterChange = (e) => {
+        setregisterField({
+            ...registerField,
+            [e.target.name]: e.target.value
+        })
+    }
     const HandleRegistration = (e) => {
         e.preventDefault()
-
-        createUserWithEmailAndPassword(authentication, 'jk@gmail.com', 'Welc0me@123$#').then(userCredentials => {
+        const { email, password, confirmpassword } = registerField
+        createUserWithEmailAndPassword(authentication, email, password).then(userCredentials => {
             const user = userCredentials.user
             console.log(user)
+        }).catch(err => {
+            console.log(err)
         })
     }
     return (
-        <form onSubmit={HandleRegistration}>
-            <button type='submit'>Submit</button>
-        </form>
+        <div className="register centered-form">
+            <Container>
+                <Card variant='outlined' sx={{ width: 400, mx: "auto", padding: 4 }}>
+                    <Typography variant="h4" component="h4" align='center' sx={{ mb: 3 }}>
+                        Register
+                    </Typography>
+                    <form onSubmit={HandleRegistration}>
+                        <TextField name='email' label="Email" variant="outlined" sx={{ width: 1, mb: 2 }}
+                            required onChange={HandleRegisterChange} />
+                        <TextField name='password' type="password" label="Password" variant="outlined" sx={{ width: 1, mb: 2 }} required onChange={HandleRegisterChange} />
+                        <TextField name='confirmpassword' type="password" label="Confirm Password" variant="outlined" sx={{ width: 1, mb: 4 }} required onChange={HandleRegisterChange} />
+                        <Button variant="outlined" type='submit'>
+                            {/* <CircularProgress size={14} /> */}
+                            Register
+                        </Button>
+                    </form>
+                    <Typography variant="p" component="p" sx={{ mt: 3 }}>
+                        Already have an account? <Link to='/login'>Click Here</Link>
+                    </Typography>
+                </Card>
+            </Container>
+        </div>
     )
 }
