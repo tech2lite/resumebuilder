cf257e9a01f07457849a09e66a839cc9e558c447
vinaykanth97 <vinaykennedy@gmail.com>
2022-12-25 23:45:38 +0530

    removed unwanted imports

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Button, TextField, FormControlLabel, Checkbox, Paper, Card, Grid, Typography } from '@mui/material';
+import { Container, Button, TextField, Card, Typography } from '@mui/material';
 import app from '../FirebaseConfig';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 import { Link, useNavigate } from 'react-router-dom';
