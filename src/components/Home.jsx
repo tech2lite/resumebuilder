c86c3a453e0d41e2cfa96e341c9bfb0f87f91a51
sandy-1430 <73515551+sandy-1430@users.noreply.github.com>
2023-01-15 12:06:53 +0530

    educationdetails

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ import { Box } from '@mui/system';
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router'
 import { dataRef } from '../FirebaseConfig';
+import EducationDetails from './EducationDetails';
 import Navbar from './Navbar';
 import PersonalInfo from './PersonalInfo';
 
@@ -38,6 +39,7 @@ export default function Home() {
             <Container sx={{ maxWidth: "60%" }} maxWidth={false}>
                 <Card sx={{ p: 2, marginTop: 5 }}>
                     <PersonalInfo />
+                    <EducationDetails />
                 </Card>
             </Container>
 
