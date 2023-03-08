022f23e078238f85a06371790c5e67a34fc6bef7
sandy-1430 <73515551+sandy-1430@users.noreply.github.com>
2023-03-09 00:58:31 +0530

    Educationdetails completed

--- src/components/EducationDetails.jsx
+++ src/components/EducationDetails.jsx
@@ -1,29 +1,22 @@
 import { Button } from '@mui/material';
 import TextField from '@mui/material/TextField';
-import { useState } from 'react';
-import { useForm, Controller } from "react-hook-form";
+import { useForm, Controller, useFieldArray } from "react-hook-form";
 import { dataRef } from '../FirebaseConfig';
 import GetLoggedUserInfo from '../reusable-hooks/GetLoggedUserInfo';
 
 export default function EducationDetails() {
     let currentUserAuthData = GetLoggedUserInfo()[0]
     let retrievedInfo = GetLoggedUserInfo()[1]
-    const { handleSubmit, control } = useForm({ values: retrievedInfo?.educationDetails });
-    const [qualify, setQualify] = useState([
-        {
-            academic: "",
-            course: "",
-            year: ""
-        }
-    ]);
-
-    const AddEducation = () => {
-        setQualify([...qualify, { academic: "", course: "", year: "" }]);
-    }
+    const { handleSubmit, register, control } = useForm({ values: retrievedInfo?.educationDetails });
+    const { fields, append, remove } = useFieldArray({
+        control,
+        name: "educationDetails",
+    });
 
     const onSubmit = data => {
         console.log(data);
-        let { uid } = currentUserAuthData
+        let { uid } = currentUserAuthData;
+        console.log(uid);
         dataRef.ref(`userInfo/${uid}`).update({
             educationDetails: data
         })
@@ -34,99 +27,43 @@ export default function EducationDetails() {
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="title">
                     <h2>Education Details</h2>
-                    <Button variant="outlined" onClick={AddEducation} type='button'>
+                    <Button variant="outlined" onClick={() => append({ academic: "", course: "", year: "" })} type='button'>
                         Add
                     </Button>
                 </div>
-                {
-                    qualify.map((index) => {
-                        const fieldName = `education[${index}]`;
-                        return (
-                            <div key={index}>
-                                <Controller
-                                    name={`${fieldName}.academic`}
-                                    control={control}
-                                    defaultValue=""
-                                    rules={{
-                                        required: 'School/College Name is required',
-                                        minLength: {
-                                            value: 2,
-                                            message: "Name must be atleast 2 Characters"
-                                        }
-                                    }}
-                                    render={({ field: { onChange, value }, fieldState: { error } }) => (
-                                        <TextField
-                                            name={`${fieldName}.academic`}
-                                            label="School/College Name"
-                                            variant="outlined"
-                                            value={value}
-                                            sx={{ width: 1, mb: 2 }}
-                                            onChange={onChange}
-                                            error={!!error}
-                                            helperText={error ? error.message : null}
-                                        />
-                                    )}
-                                />
-                                <Controller
-                                    name={`${fieldName}.course`}
-                                    control={control}
-                                    defaultValue=""
-                                    rules={{
-                                        required: 'course Name is required',
-                                        minLength: {
-                                            value: 2,
-                                            message: "Name must be atleast 2 Characters"
-                                        }
-                                    }}
-                                    render={({ field: { onChange, value }, fieldState: { error } }) => (
+                {fields.map((items, index) => (
+                    <div key={items.id}>
+                        <Controller
+                            name={`education[${index}].academic`}
+                            control={control}
+                            render={({ field }) => <TextField label="Academic" sx={{ width: 1, mb: 2 }} {...field} />}
+                        />
+                        <Controller
+                            name={`education[${index}].course`}
+                            control={control}
+                            render={({ field }) => <TextField label="Course" sx={{ width: 1, mb: 2 }} {...field} />}
+                        />
+                        <Controller
+                            name={`education[${index}].year`}
+                            control={control}
+                            render={({ field }) => <TextField label="Year" sx={{ width: 1, mb: 2 }} {...field} />}
+                        />
 
-                                        <TextField
-                                            name={`${fieldName}.course`}
-                                            label="Course Name"
-                                            variant="outlined"
-                                            value={value}
-                                            sx={{ width: 1, mb: 2 }}
-                                            onChange={onChange}
-                                            error={!!error}
-                                            helperText={error ? error.message : null}
-                                        />
-                                    )}
-                                />
-                                <Controller
-                                    name={`${fieldName}.year`}
-                                    control={control}
-                                    defaultValue=""
-                                    rules={{
-                                        required: 'year is required',
-                                        minLength: {
-                                            value: 4,
-                                            message: "Correct year is required"
-                                        }
-                                    }}
-                                    render={({ field: { onChange, value }, fieldState: { error } }) => (
-                                        <TextField
-                                            name={`${fieldName}.year`}
-                                            label="Passed Year"
-                                            variant="outlined"
-                                            multiline
-                                            maxRows={4}
-                                            value={value}
-                                            sx={{ width: 1, mb: 2 }}
-                                            onChange={onChange}
-                                            error={!!error}
-                                            helperText={error ? error.message : null}
-                                        />
-                                    )}
-                                />
-                            </div>
-                        )
-                    })
-                }
-
+                        <Button
+                            type='button'
+                            variant='contained'
+                            color='error'
+                            sx={{ mb: 2 }}
+                            onClick={() => remove(index)}
+                        >
+                            Remove
+                        </Button>
+                    </div>
+                ))}
                 <Button variant="outlined" type='submit'>
                     Save
                 </Button>
-            </form>
-        </div>
+            </form >
+        </div >
     )
 }
